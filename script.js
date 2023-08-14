f7cccb3f365d603180ff2a8ac691aafd6289615b
Stephany Eloise <stephany.eloise@hotmail.com>
2023-08-14 12:36:15 -0300

    Almost done

--- script.js
+++ script.js
@@ -2,22 +2,34 @@ const screen1 = document.getElementById("screen1");
 const screen2 = document.getElementById("screen2");
 
 const submitBtn = document.getElementById("submit-btn");
-const result = document.getElementById('result');
-
 const items = document.querySelectorAll(".item");
+const result = document.getElementById("score-value");
+
+let dataIndex = 0
 
 items.forEach(item => {
     item.addEventListener("click", scoreSelection)
 });
 
-submitBtn.addEventListener("click", () => {
-    screen1.style.display = "none";
-    screen2.style.display = "block";
-});
 
 function scoreSelection(evt) {
     const targetClicked = evt.target
-    const valueSelected = targetClicked.attributes['data-index'].value
-    console.log(targetClicked.classList.add('selected'))
+    dataIndex = targetClicked.attributes['data-index'].value
     
-}
+    items.forEach(item => {
+        item.classList.remove("selected");
+    });
+    
+    targetClicked.classList.add("selected");
+    }
+
+    items.forEach(item => {
+        item.addEventListener("click", scoreSelection);
+});
+
+submitBtn.addEventListener("click", () => {
+    screen1.style.display = "none";
+    screen2.style.display = "flex";
+    
+    result.textContent = "You selected " + dataIndex + " out of 5";
+});
